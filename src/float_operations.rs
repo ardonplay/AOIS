7527c3ad24c020ee639ffb0659a81bde4eefa2d0
Vladimir Moshchuk <ardonplay@Vladimirs-MacBook-Air.local>
2023-03-02 20:11:23 +0300

    finally...it works well!!!

---
+++ src/float_operations.rs
@@ -0,0 +1,98 @@
+pub mod floating_operations {
+    use std::cmp::Ordering;
+
+    use crate::operations::binary_operations::{binary_sum, to_binary, to_decimal};
+
+    pub struct Float {
+        sign: i8,
+        mantissa: String,
+        index: String,
+
+    }
+
+    impl Clone for Float {
+        fn clone(&self) -> Float {
+            Float {
+                sign: self.sign.clone(),
+                mantissa: self.mantissa.clone(),
+                index: self.index.clone(),
+            }
+        }
+    }
+
+    pub fn print_float_bi_code(float: Float) {
+        println!("{}|{}|{}\n", float.sign, float.index, float.mantissa);
+    }
+
+    pub fn decimal_to_float(mut decimal: f32) -> Float {
+        let mut output = Float { mantissa: String::new(), index: String::new(), sign: 0 };
+
+        let decimal_string = String::from(decimal.to_string());
+        if decimal < 0 as f32 {
+            output.sign = 1;
+            decimal = -decimal;
+        }
+
+        let mut index = decimal_string.chars().count() - 1;
+
+        let mut ind = 0;
+
+        while index > 0 {
+            if decimal_string.chars().nth(index).unwrap() == '.' {
+                break;
+            } else {
+                ind += 1;
+            }
+            index -= 1;
+        }
+
+        let decimal_int = (decimal * f32::powf(10.0, ind as f32)) as i32;
+        output.index = to_binary(ind, 8);
+        output.mantissa = to_binary(decimal_int, 23);
+        output
+    }
+
+    pub fn basing(mut first: Float, second: Float, first_index: i32) -> Float {
+        while first.index.clone() != second.index.clone() {
+            first.index = binary_sum(first.index.clone(), to_binary(1, 8));
+        }
+        let mut digit_mantissa = to_decimal(first.mantissa.clone(), 23);
+        digit_mantissa *= i32::pow(10, (to_decimal(first.index.clone(), 8) - first_index) as u32);
+        first.mantissa = to_binary(digit_mantissa, 23);
+        first
+    }
+
+    pub fn binary_float_sum(mut first: Float, mut second: Float) {
+        let mut res = Float { mantissa: String::new(), index: String::new(), sign: 0 };
+
+        let second_index = to_decimal(second.index.clone(), 8);
+        let first_index = to_decimal(first.index.clone(), 8);
+
+
+        match first_index.cmp(&second_index) {
+            Ordering::Less => {
+                first = basing(first.clone(), second.clone(), first_index)
+            }
+            Ordering::Equal => {}
+            Ordering::Greater => {
+                second = basing(second.clone(), first.clone(), second_index)
+            }
+        }
+
+
+        if first.sign == 1 && second.sign == 1 {
+            res.sign = 1;
+            first.mantissa.replace_range(0..1, "1");
+            second.mantissa.replace_range(0..1, "1");
+        } else if first.sign == 1 {
+            first.mantissa.replace_range(0..1, "1");
+        } else if second.sign == 1 {
+            second.mantissa.replace_range(0..1, "1");
+        }
+        res.index = binary_sum(first.index, second.index);
+        res.mantissa = binary_sum(first.mantissa, second.mantissa);
+        let rezulator: f32 = to_decimal(res.clone().mantissa, 23) as f32 * f32::powf(10.0, (-to_decimal(res.clone().index, 8) / 2) as f32);
+        println!("{}", rezulator);
+        print_float_bi_code(res);
+    }
+}
---
+++ src/operations.rs
@@ -0,0 +1,236 @@
+pub mod binary_operations {
+    pub fn to_binary(mut input: i32, bin_size: i32) -> String {
+        let mut output = String::new();
+
+        let sign = input;
+
+        input = input.abs();
+
+        loop {
+            if input > 0 {
+                let x: u32 = (input % 2) as u32;
+                let temp = x.to_string();
+
+                output.push_str(&temp);
+                input /= 2;
+            } else {
+                break;
+            }
+        }
+        let space = bin_size - 1 - output.chars().count() as i32;
+        if space > 0 {
+            for _i in 0..space {
+                output.push('0');
+            }
+        }
+
+        if sign.is_negative() {
+            output.push('1');
+        } else if sign.is_positive() {
+            output.push('0');
+        }
+        if sign == 0 {
+            output.push('0');
+        }
+        output.chars().rev().collect()
+    }
+
+    pub fn to_decimal(input: String, bin_size: i32) -> i32 {
+        let mut output: i32 = 0;
+
+        let mut degree: i32 = bin_size - 2;
+
+        let sign = input.chars().next().unwrap();
+
+        let mut chars = input.chars();
+
+        chars.next();
+
+        for i in chars {
+            let bit: i32 = i.to_digit(10).unwrap() as i32;
+
+            let block = bit * i32::pow(2, degree as u32);
+            output += block;
+            degree -= 1;
+        }
+        if sign == '1' {
+            output *= -1;
+        }
+        output
+    }
+
+    pub fn to_rev_view(input: String, bin_size: i32) -> String {
+        let mut output = String::with_capacity(bin_size as usize);
+        output.push(input.chars().next().unwrap());
+        for i in 1..bin_size {
+            if input.chars().nth(i as usize).unwrap() == '0' {
+                output.push('1')
+            } else if input.chars().nth(i as usize).unwrap() == '1' {
+                output.push('0');
+            }
+        }
+        output
+    }
+
+    pub fn to_ext_view(input: String, bin_size: i32) -> String {
+        summator(to_rev_view(input, bin_size), to_binary(1, bin_size))
+    }
+
+
+    pub fn binary_sum(mut first: String, mut second: String) -> String {
+        let sign = get_sign(first.clone(), second.clone());
+        let mut rev_first = false;
+        let mut rev_second = false;
+
+        let bin_size = first.chars().count();
+        if first.starts_with('1') && second.starts_with('1') {
+            first = to_ext_view(first, bin_size as i32);
+            second = to_ext_view(second, bin_size as i32);
+        } else if first.starts_with('1') {
+            first = to_ext_view(first, bin_size as i32);
+            //second = to_ext_view(second);
+            rev_first = true;
+        } else if second.starts_with('1') {
+            second = to_ext_view(second, bin_size as i32);
+            //first = to_ext_view(first);
+            rev_second = true;
+        }
+        let mut sum = summator(first, second);
+
+        if sum.starts_with('1') {
+            sum = from_addition_to_reverse(sum, bin_size as i32);
+            sum = to_rev_view(sum, bin_size as i32)
+        }
+
+        sum
+    }
+
+
+    pub fn summator(first: String, second: String) -> String {
+        let mut sum = String::new();
+
+
+        let mut trans: bool = false;
+
+
+        let mut index = first.chars().count();
+
+
+        loop {
+            if index == 0 {
+                break;
+            }
+            let first_binary_symbol = first.chars().nth(index - 1).unwrap();
+            let second_binary_symbol = second.chars().nth(index - 1).unwrap();
+
+            if first_binary_symbol == '0' && second_binary_symbol == '0' {
+                if trans {
+                    sum.push('1');
+                    trans = false;
+                } else if !trans {
+                    sum.push('0');
+                    trans = false;
+                }
+            } else if (first_binary_symbol == '1' && second_binary_symbol == '0') ||
+                (first_binary_symbol == '0' && second_binary_symbol == '1') {
+                match trans {
+                    true => {
+                        sum.push('0');
+                        trans = true;
+                    }
+                    false => {
+                        sum.push('1');
+                        trans = false;
+                    }
+                }
+            } else if first_binary_symbol == '1' && second_binary_symbol == '1' {
+                if !trans {
+                    sum.push('0');
+                    trans = true;
+                } else if trans {
+                    sum.push('1');
+                    trans = true;
+                }
+            }
+
+            index -= 1;
+        }
+        sum.chars().rev().collect()
+    }
+
+    pub fn from_addition_to_reverse(input: String, bin_size: i32) -> String {
+        let mut output = input.clone();
+        let sign = input.chars().next().unwrap();
+        if sign == '1' {
+            output = summator(input.chars().collect(), to_ext_view(to_binary(-1, bin_size), bin_size));
+        }
+        output
+    }
+
+    pub fn get_sign(first: String, second: String) -> char {
+        let mut sign: char = '0';
+        if first.starts_with('0') && second.starts_with('0') {
+            sign = '0';
+        } else if (first.starts_with('0') && second.starts_with('1')) || (first.starts_with('1') && second.starts_with('0')) {
+            sign = '1';
+        } else if first.starts_with('1') && second.starts_with('1') {
+            sign = '2';
+        }
+
+        sign
+    }
+
+    pub fn multiplex(first: String, second: String) -> String {
+        let mut iter = to_binary(1, 32);
+
+        let mut output = first.clone();
+        let sign = get_sign(first.clone(), second.clone());
+        let mut end = second;
+
+        end.replace_range(0..1, "0");
+        println!("{}", sign);
+        loop {
+            if iter != end {
+                output = binary_sum(output, first.clone());
+            } else {
+                break;
+            }
+            iter = summator(iter, to_binary(1, 32));
+        }
+        if sign == '1' {
+            output.replace_range(0..1, "1");
+        } else if sign == '2' {
+            output.replace_range(0..1, "0");
+        }
+        output
+    }
+
+    pub fn divide(mut first: String, mut second: String) -> String {
+        let sign = get_sign(first.clone(), second.clone());
+        first.replace_range(0..1, "0");
+
+
+        if second == to_binary(0, 32) {
+            return to_binary(0, 32);
+        }
+        second.replace_range(0..1, "1");
+
+        let mut output = first.clone();
+
+        let mut iter = to_binary(0, 32);
+        loop {
+            output = binary_sum(output, second.clone());
+            if !output.starts_with(first.chars().next().unwrap()) {
+                break;
+            }
+            iter = binary_sum(iter, to_binary(1, 32));
+        }
+
+        if sign == '1' {
+            iter.replace_range(0..1, "1");
+        }
+
+
+        iter
+    }
+}
