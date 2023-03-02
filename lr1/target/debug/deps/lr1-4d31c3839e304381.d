70ed3e9b48a650d9e3334f12f58876d04efa2d1a
Vladimir Moshchuk <ardonplay@Vladimirs-MacBook-Air.local>
2023-03-02 20:14:34 +0300

    move lr1 to lr1 dir

---
+++ lr1/target/debug/deps/lr1-4d31c3839e304381.d
@@ -0,0 +1,10 @@
+/Users/ardonplay/Developer/AOIS/lr1/target/debug/deps/lr1-4d31c3839e304381.rmeta: src/main.rs src/operations.rs src/float_operations.rs Cargo.toml
+
+/Users/ardonplay/Developer/AOIS/lr1/target/debug/deps/lr1-4d31c3839e304381.d: src/main.rs src/operations.rs src/float_operations.rs Cargo.toml
+
+src/main.rs:
+src/operations.rs:
+src/float_operations.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
