09669c638d38d24122866692b4a5cd8c137cc257
Vladimir <ardonplay@Vladimirs-iMac.local>
2023-04-23 11:58:41 +0300

    finally working analyze method

--- lr3/src/main/java/org/ardonplay/aois/AnalyticalMinimize.java
+++ lr3/src/main/java/org/ardonplay/aois/AnalyticalMinimize.java
@@ -4,9 +4,6 @@ import org.ardonplay.logic.LogicalOperations;
 
 import java.util.*;
 
-import static org.ardonplay.logic.LogicalOperations.conjunction;
-import static org.ardonplay.logic.LogicalOperations.disjunction;
-
 public class AnalyticalMinimize {
     private final Map<String, Integer> symbols = new HashMap<>();
 
@@ -18,6 +15,49 @@ public class AnalyticalMinimize {
             } else {
                 symbols.put(variable, 1);
                 symbols.put("!" +variable, 0);
+            }
+        }
+    }
+
+    private int stringSum(String first, String second){
+        if(Objects.equals(first, "!" + second) ||
+                Objects.equals("!" + first, second)){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    private void indexChecker(List<Object> indexes){
+        for(int i =0; i < indexes.size() -1; i++){
+            Object current = indexes.get(i);
+            Object next = indexes.get(i+1);
+
+            if(current instanceof String && next instanceof String){
+                if(Objects.equals(current, next)){
+                    indexes.remove(i+1);
+                }
+                else {
+                    indexes.set(i, stringSum((String) current, (String) next));
+                    indexes.remove(i + 1);
+                }
+                i--;
+            }
+            else if(current instanceof Integer && next instanceof Integer){
+                indexes.set(i, (int) current + (int) next);
+                indexes.remove(i + 1);
+                i--;
+            }
+            else if(current instanceof Integer && next instanceof String){
+                indexes.set(i, next);
+                indexes.remove(i + 1);
+                i--;
+            }
+            else if(current instanceof String && next instanceof Integer){
+                indexes.set(i, current);
+                indexes.remove(i + 1);
+                i--;
             }
         }
     }
@@ -56,23 +96,23 @@ public class AnalyticalMinimize {
     }
 
     public List<List<String>> minimiseSecond(List<List<String>> constituents, boolean sdnf) {
-        List<List<String>> output = new ArrayList<>(constituents);
+        List<List<String>> output = new ArrayList<>();
 
-        for (int i = 0; i < output.size(); i++) {
-            addSymbols(output.get(i));
+        for (int i = 0; i < constituents.size(); i++) {
+            addSymbols(constituents.get(i));
 
             List<Object> indexes = new ArrayList<>();
 
-            for (List<String> strings : output) {
-                if (output.get(i) != strings) {
+            for (List<String> strings : constituents) {
+                if (constituents.get(i) != strings) {
                     solve(strings, indexes, sdnf);
                 }
             }
-            System.out.println(symbols);
-            System.out.println(indexes);
+            indexChecker(indexes);
 
-
-
+            if(indexes.get(0) instanceof String){
+              output.add(constituents.get(i));
+            }
             symbols.clear();
         }
         return output;
