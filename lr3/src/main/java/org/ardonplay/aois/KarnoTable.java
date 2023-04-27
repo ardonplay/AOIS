4f64c474aab442bbc8efb106eea3b0f36c56692b
Vladimir Moshchuk <ardonplay@Vladimirs-MacBook-Air.local>
2023-04-27 12:13:10 +0300

    finally, karno

--- lr3/src/main/java/org/ardonplay/aois/KarnoTable.java
+++ lr3/src/main/java/org/ardonplay/aois/KarnoTable.java
@@ -3,6 +3,7 @@ package org.ardonplay.aois;
 import org.ardonplay.aois.KarnoData.DataSet;
 import org.ardonplay.aois.KarnoData.DataTable;
 
+import javax.xml.crypto.Data;
 import java.util.*;
 
 public class KarnoTable {
@@ -27,7 +28,7 @@ public class KarnoTable {
     }
 
 
-    public List<List<String>> minimize(Map<List<Integer>, Integer> logicalTable){
+    public List<List<String>> minimize(Map<List<Integer>, Integer> logicalTable, List<String> symbols){
         List<List<String>> output = new ArrayList<>();
 
         int[][] karnoTable = new int[2][4];
@@ -45,34 +46,56 @@ public class KarnoTable {
         for(DataTable dataTable: dataSet.getTables()){
             if (table.contains(dataTable)){
                 if(!figures.isEmpty()) {
-                    List<DataTable> tempFigures = new ArrayList<>();
+                    List<Boolean> booleans = new ArrayList<>();
                     for (DataTable figure : figures) {
-                        System.out.println(figure.contains(dataTable));
-                        System.out.println(Arrays.deepToString(dataTable.table()));
-                        if (!figure.contains(dataTable)) {
-                            tempFigures.add(dataTable);
-                        }
-                        else {
-                            break;
-                        }
+                        booleans.add(figure.contains(dataTable));
                     }
-                    figures.addAll(tempFigures);
+                    if(!booleans.contains(true)){
+                        figures.add(dataTable);
+                    }
                 }
                 else {
                     figures.add(dataTable);
                 }
             }
         }
-//        for(DataTable table1: figures){
-//            System.out.println(Arrays.deepToString(table1.table()));
-//        }
 
-//        for (int[] ints : karnoTable) {
-//            for (int anInt : ints) {
-//                System.out.print(anInt + " ");
-//            }
-//            System.out.println();
-//        }
+
+
+        for(DataTable dataTable: figures){
+            List<List<Integer>> indexes = new ArrayList<>();
+            for(int i =0; i < dataTable.table().length; i++){
+                for(int j =0; j < dataTable.table()[i].length; j++){
+                    if(dataTable.table()[i][j] == 1){
+                        indexes.add(logicalIndexes.get(Arrays.asList(i, j)));
+                    }
+                }
+            }
+
+
+            List<Integer> commonIndexes = new ArrayList<>();
+
+
+            for (int i = 0; i < indexes.get(0).size(); i++) {
+                boolean isCommon = true;
+                int commonValue = indexes.get(0).get(i);
+                for (int j = 1; j < indexes.size(); j++) {
+                    if (indexes.get(j).get(i) != commonValue) {
+                        isCommon = false;
+                        break;
+                    }
+                }
+                if (isCommon) {
+                    commonIndexes.add(i);
+                }
+            }
+            List<String> stringIndex = new ArrayList<>();
+            for(Integer index: commonIndexes){
+               stringIndex.add(symbols.get(index));
+            }
+            output.add(stringIndex);
+
+        }
 
         return output;
     }
--- lr3/src/main/java/org/ardonplay/aois/Main.java
+++ lr3/src/main/java/org/ardonplay/aois/Main.java
@@ -10,49 +10,30 @@ public class Main {
     public static void main(String[] args) {
         LogicalExpressionSolver solver = new LogicalExpressionSolver("(A*B)+C");
 
+        System.out.println(solver);
+       AnalyticalMinimize minimize = new AnalyticalMinimize();
+
+       List<List<String>> pdnf = solver.getPdnf().getNormalForm();
+       List<List<String>> minimization = minimize.minimise(pdnf);
+       System.out.println(minimization);
+       System.out.println(minimize.minimiseSecond(minimization, true));
+
+
+
+        minimization = minimize.minimise(solver.getPcnf().getNormalForm());
+        System.out.println(minimization);
+        System.out.println(minimize.minimiseSecond(minimization, false));
+
+        TableMinimize tableMinimize = new TableMinimize();
+
+        System.out.println(tableMinimize.tableMinimize(solver.getPdnf().getNormalForm()));
+
+        System.out.println(tableMinimize.tableMinimize(solver.getPcnf().getNormalForm()));
 
         KarnoTable karnoTable = new KarnoTable();
 
 
-        karnoTable.minimize(solver.getLogicalTable());
+        System.out.println(karnoTable.minimize(solver.getLogicalTable(), LogicalExpressionSolver.getAllSymbols()));
 
-
-
-
-       //System.out.println(logicalTable);
-//
-//
-//        System.out.println(solver);
-//        AnalyticalMinimize minimize = new AnalyticalMinimize();
-//
-//        List<List<String>> pdnf = solver.getPdnf().getNormalForm();
-//        List<List<String>> minimization = minimize.minimise(pdnf);
-//        System.out.println(minimization);
-//        System.out.println(minimize.minimiseSecond(minimization, true));
-//
-//
-//
-//        minimization = minimize.minimise(solver.getPcnf().getNormalForm());
-//        System.out.println(minimization);
-//        System.out.println(minimize.minimiseSecond(minimization, false));
-//
-//
-//        TableMinimize tableMinimize = new TableMinimize();
-//
-//        System.out.println(tableMinimize.tableMinimize(solver.getPdnf().getNormalForm()));
-//
-//        System.out.println(tableMinimize.tableMinimize(solver.getPcnf().getNormalForm()));
-
-        DataTable dataTable = new DataTable(new int[][]{
-                {0,1,1,0}, {0, 1, 1, 0}});
-
-        DataTable dataTable1 = new DataTable(new int[][]{{0,1,0,0}, {0,0,0,0}});
-
-//        System.out.println(Arrays.deepToString(dataTable1.table()));
-//
-//        System.out.println(Arrays.deepToString(dataTable.table()));
-
-        System.out.println(dataTable.contains(dataTable1));
-//        System.out.println(dataTable.contains(dataTable1));
     }
 }
