07a61b985c2ff82a02a16db38f3e204b461aa83b
Vladimir Moshchuk <ardonplay@Vladimirs-MacBook-Air.local>
2023-05-11 19:00:26 +0300

    add 50% of map

--- lr6/src/main/kotlin/Main.kt
+++ lr6/src/main/kotlin/Main.kt
@@ -2,10 +2,9 @@ import hashTable.HashTable
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    val testVal = HashTable<Int, String>()
+    val testVal = HashTable<String, String>()
 
-    testVal.put(10, "Hi")
-    testVal.put(10, "Hг")
-    println(testVal[10])
-    println(testVal)
+    testVal.put("hi", "hello")
+    println(testVal["hi"])
+    println(testVal.containsValue("hello"))
 }
--- lr6/src/main/kotlin/hashTable/HashTable.kt
+++ lr6/src/main/kotlin/hashTable/HashTable.kt
@@ -1,45 +1,123 @@
 package hashTable
 
+import java.util.*
 import java.util.Objects.hash
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
+import kotlin.math.abs
 
-class HashTable<K, V>(private val capacity: Int = 16): Map<K, V>{
-    private val nodes = ArrayList<V?>(capacity).apply {
-        for (i in 0 until capacity) {
-            add(null)
+class HashTable<K, V>(private val capacity: Int = 16) : Map<K, V> {
+
+    private var list: Array<LinkedList<Node<K, V>>?> = Array(size = capacity) { LinkedList() }
+
+    private class Node<K, V>(val key: K, val value: V) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Node<*, *>
+
+            return key == other.key
+        }
+
+        override fun hashCode(): Int {
+            return key?.hashCode() ?: 0
         }
     }
-    override val entries: Set<Map.Entry<K, V>>
-        get() = TODO("Not yet implemented")
-    override val keys: Set<K>
-        get() = TODO("Not yet implemented")
+
+    class Entry<K, V>(override val key: K, override var value: V) : Map.Entry<K, V> {
+
+        override fun toString(): String {
+            return "$key=$value"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Entry<*, *>
+
+            return value == other.value
+        }
+
+        override fun hashCode(): Int {
+            return value?.hashCode() ?: 0
+        }
+    }
+
+    override var entries: Set<Map.Entry<K, V>> = HashSet(capacity)
+
+    override var keys: Set<K> = HashSet(capacity)
     override val size: Int
-        get() = nodes.size
-    override val values: Collection<V>
-        get() = TODO("Not yet implemented")
+        get() = list.size
+
+    override val values: Collection<V> = ArrayList(capacity)
 
     override fun isEmpty(): Boolean {
-        return nodes.size == 0
-    }
-    private fun getHash(key: K): Int{
-       return hash(key) % capacity
+        return list.isEmpty()
     }
 
-    fun put(key: K, value: V){
-        nodes.add(getHash(key), value)
+    private fun getHash(key: K): Int {
+        return when (key) {
+            Int -> getHash(key as Int)
+            String -> getHash(key as String)
+            else -> hash(key)
+        }
     }
-    override fun get(key: K): V? {
-        return nodes.getOrNull(getHash(key))
+
+    private fun getHash(key: Int): Int {
+        return key % size
+    }
+
+    private fun getHash(key: String): Int {
+        var hash = 0
+        for (symbol in key) {
+            hash = (hash shl 5) - hash + (symbol.code)
+        }
+        return abs(hash)
+    }
+
+    private fun getIndex(key: K): Int {
+        return getHash(key) % capacity
+    }
+
+    fun put(key: K, value: V) {
+        val index = getIndex(key)
+        val element = list.getOrNull(index)
+        if (element != null) {
+            keys += key
+            entries += Entry(key, value)
+
+            if (!element.contains(Node(key, value))) {
+                list[getIndex(key)]?.add(Node(key, value))
+            } else {
+                list[getIndex(key)]?.remove(Node(key, value))
+                list[getIndex(key)]?.add(Node(key, value))
+            }
+        }
+    }
+
+    override fun get(key: K): V {
+        val element = list.getOrNull(getIndex(key))
+        if (element != null) {
+            for (node in element) {
+                if (node.key == key) {
+                    return node.value
+                }
+            }
+        }
+        throw NullPointerException("map dont have this key!")
     }
 
     override fun containsValue(value: V): Boolean {
-        TODO("Not yet implemented")
+        return entries.any{it.value == value}
     }
 
     override fun containsKey(key: K): Boolean {
-        TODO("Not yet implemented")
+        return keys.contains(key)
     }
 
     override fun toString(): String {
-        return nodes.toString()
+        return entries.toString()
     }
 }
