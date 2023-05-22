d630dc83d7eb6edb932145eb96be6fde4a59495a
Vladimir Moshchuk <ardonplay@gmail.com>
2023-05-22 09:07:42 +0300

    update

--- lr7/src/main/java/org/ardonplay/aois/lr7/Main.java
+++ lr7/src/main/java/org/ardonplay/aois/lr7/Main.java
@@ -2,7 +2,6 @@ package org.ardonplay.aois.lr7;
 
 public class Main {
     public static void main(String[] args) {
-        Binary binary2 = new Binary(11);
-        System.out.println(binary2);
-        }
+        System.out.println("Hello world!");
     }
+}
